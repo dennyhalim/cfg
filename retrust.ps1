18af15b5e641f9e414df17252666f5ecbc06494b
DennyHalim.com <mail2dny@gmail.com>
2017-06-22 11:08:23 +0700

    Update retrust.ps1

--- retrust.ps1
+++ retrust.ps1
@@ -1,3 +1,4 @@
+#dennyhalim.com #repairing domain trust relationship problem
 if(!(Test-ComputerSecureChannel)) {Test-ComputerSecureChannel -Repair}
 if(!(Test-ComputerSecureChannel)) {Reset-ComputerMachinePassword}
 nltest.exe /sc_reset:%USERDNSDOMAIN% ||  nltest /sc_change_pwd:%USERDNSDOMAIN%
