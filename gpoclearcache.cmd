f62feddda497829bca0352e85fa452dc07400f2d
DennyHalim.com <mail2dny@gmail.com>
2017-06-22 10:42:17 +0700

    Create gpoclearcache.cmd

---
+++ gpoclearcache.cmd
@@ -0,0 +1,20 @@
+cd "%ALLUSERSPROFILE%\Microsoft\Group Policy" && rd History.old /s /q
+attrib -r -h -s History
+move /y History History.old
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies.old" /f
+REG DELETE "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies.old" /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\History.old" /f
+REG DELETE "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\History.old" /f
+REG copy "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies" "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies.old" /s /f
+REG copy "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies" "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies.old" /s /f
+REG copy "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\History" "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\History.old" /s /f
+REG copy "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\History" "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\History.old" /s /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies" /f
+REG DELETE "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies" /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\History" /f
+REG DELETE "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\History" /f
+DEL /F /Q C:\WINDOWS\security\Database\secedit.sdb.old
+move /y %windir%\security\Database\secedit.sdb %windir%\security\Database\secedit.sdb.old
+Klist purge
+pause 'please restart computer'
+rem gpupdate /force
