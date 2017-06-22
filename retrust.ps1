0f94157607bb2c71a91dcea74da403cef17877cd
DennyHalim.com <mail2dny@gmail.com>
2017-06-22 11:06:21 +0700

    Create retrust.ps1

---
+++ retrust.ps1
@@ -0,0 +1,4 @@
+if(!(Test-ComputerSecureChannel)) {Test-ComputerSecureChannel -Repair}
+if(!(Test-ComputerSecureChannel)) {Reset-ComputerMachinePassword}
+nltest.exe /sc_reset:%USERDNSDOMAIN% ||  nltest /sc_change_pwd:%USERDNSDOMAIN%
+nltest.exe /sc_verify:%USERDNSDOMAIN%
