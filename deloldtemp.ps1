8a17ed0e198b58096f3a068e130ff8a0effaa62f
DennyHalim.com <mail2dny@gmail.com>
2017-06-25 20:44:58 +0700

    Update deloldtemp.ps1

--- deloldtemp.ps1
+++ deloldtemp.ps1
@@ -1,2 +1,5 @@
+#dennyhalim.com #removing ALL files in each and every users' %temp% folder older than 70 days
+#DANGER!!! anything will be removed without warning. no question asked!
+
 Get-ChildItem -Path "C:\Users\*\AppData\Local\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).Add
-Days(-70)} | Remove-Item -Force
+Days(-70)} | Remove-Item -Force -Recurse
