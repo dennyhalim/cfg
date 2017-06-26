130ad592ecab1b527b414541c416906d6e3e7863
DennyHalim.com <mail2dny@gmail.com>
2017-06-27 05:34:44 +0700

    Update deloldtemp.ps1

--- deloldtemp.ps1
+++ deloldtemp.ps1
@@ -1,8 +1,13 @@
-#dennyhalim.com #removing ALL files in each and every users' AND windows' %temp% folder older than 70 days
+#dennyhalim.com #removing ALL files with following conditions:
+# folder name: Temp
+# location: %temp% and %windir%\temp
+# files/folders older than 70 days
+# remove without warning
+# remove the option '-WhatIf' to perform real action
 #WARNING!! DANGER!!! anything will be removed without warning. no question asked!
 
-Get-ChildItem -Path "$env:public\..\*\AppData\Local\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse
-Get-ChildItem -Path "$env:windir\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse
+Get-ChildItem -Path "$env:public\..\*\AppData\Local\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse -WhatIf
+Get-ChildItem -Path "$env:windir\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse -WhatIf
 
 #cmd:
 #forfiles.exe -p %temp% -s -m * -d -70 -c "cmd /c echo @path"
