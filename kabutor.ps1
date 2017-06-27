18c97ef147d8914357bc17ed01c146d1b91f4c54
DennyHalim.com <mail2dny@gmail.com>
2017-06-27 16:42:45 +0700

    Update kabutor.ps1

--- kabutor.ps1
+++ kabutor.ps1
@@ -1,1 +1,1 @@
-$installer = "$env:windir\temp\kabutoinstaller.exe"; Invoke-WebRequest -Uri https://app.kabuto.io/dl/c/w1gowhcgaw-zmu0 -OutFile $installer
+$installer = "$env:windir\temp\kabutoinstaller.exe"; Invoke-WebRequest -Uri https://app.kabuto.io/dl/c/w1gowhcgaw-zmu0 -OutFile $installer; . $installer
