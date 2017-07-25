f870a0f37db92db2bacd2abdac090c6453f0b3bd
DennyHalim.com <mail2dny@gmail.com>
2017-07-25 16:10:53 +0700

    Update deloldtemp.ps1

--- deloldtemp.ps1
+++ deloldtemp.ps1
@@ -6,11 +6,13 @@
 # remove the option '-WhatIf' to perform real action
 #WARNING!! DANGER!!! anything will be removed without warning. no question asked!
 
-Get-ChildItem -Path "$env:public\..\*\AppData\Local\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse -WhatIf
-Get-ChildItem -Path "$env:public\..\*\AppData\Local\Google\Chrome\User Data\Default\Cache" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse -WhatIf
-Get-ChildItem -Path "$env:public\..\*\AppData\Local\Microsoft\Windows\Temporary Internet Files" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse -WhatIf
-Get-ChildItem -Path "$env:public\..\*\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache2" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse -WhatIf
 Get-ChildItem -Path "$env:windir\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse -WhatIf
+Get-ChildItem -Path "$env:public\..\*\AppData\Local\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse -WhatIf
+
+# remove ALL browser cache
+Get-ChildItem -Path "$env:public\..\*\AppData\Local\Google\Chrome\User Data\Default\Cache" -Recurse | Remove-Item -Force -Recurse -WhatIf
+Get-ChildItem -Path "$env:public\..\*\AppData\Local\Microsoft\Windows\Temporary Internet Files" -Recurse | Remove-Item -Force -Recurse -WhatIf
+Get-ChildItem -Path "$env:public\..\*\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache2" -Recurse | Remove-Item -Force -Recurse -WhatIf
 
 #cmd:
 #forfiles.exe -p %temp% -s -m * -d -70 -c "cmd /c echo @path"
