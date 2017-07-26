51bb5f6f4ca41909c69f85e11e11bd21c12de593
DennyHalim.com <mail2dny@gmail.com>
2017-07-26 09:32:00 +0700

    Update consolelogging.sh

--- consolelogging.sh
+++ consolelogging.sh
@@ -7,5 +7,5 @@ _TGL=$(date +%F-%T)
 HISTFILESIZE=10000
 HISTFILE=/var/log/console/$USER-$_TGL
 SSHFILE=/var/log/console/$USER-ssh-$_TGL
-export HISTFILE SSHFILE
+export HISTFILE HISTFILESIZE SSHFILE
 ssh () { /usr/bin/ssh "$@" | tee -a $SSHFILE ; }
