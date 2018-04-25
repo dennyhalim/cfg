1a524587bf1936c512ef4524530e6d6423b6d0d3
DennyHalim.com <mail2dny@gmail.com>
2018-04-25 14:52:11 +0700

    Update delcache.cmd

--- delcache.cmd
+++ delcache.cmd
@@ -1,2 +1,3 @@
-cd %localappdata%\Mozilla\Firefox\Profiles\*.default && rd cache2 /s /q
+cd "%localappdata%\Mozilla\Firefox\Profiles\*.default" && rd cache2 /s /q
 cd "%localappdata%\Google\Chrome\User Data\Default\" && rd cache /s /q
+cd "%localappdata%\Microsoft\Windows" && rd "Temporary Internet Files" /s /q
