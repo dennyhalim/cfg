3e358b849e4336d10dcf39e7146b3b91253bf4bd
DennyHalim.com <mail2dny@gmail.com>
2018-04-25 13:12:37 +0700

    Update deloldtemp.ps1

--- deloldtemp.ps1
+++ deloldtemp.ps1
@@ -7,13 +7,13 @@
 # WARNING!! DANGER!!! if you remomve -WhatIf option then anything will be removed without warning. no question asked!
 # if -WhatIf option remains, then nothing removed
 
-Get-ChildItem -Path "$env:windir\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse -WhatIf
-Get-ChildItem -Path "$env:public\..\*\AppData\Local\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Force -Recurse -WhatIf
+Get-ChildItem -Path "$env:windir\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Verbose -Force -Recurse -WhatIf >> "$env:windir\Logs\del.log"
+Get-ChildItem -Path "$env:public\..\*\AppData\Local\Temp" -Recurse | Where-Object{$_.LastAccessTime -lt (Get-Date).AddDays(-70)} | Remove-Item -Verbose -Force -Recurse -WhatIf >> "$env:windir\Logs\del.log"
 
 # remove ALL browser cache
-Get-ChildItem -Path "$env:public\..\*\AppData\Local\Google\Chrome\User Data\Default\Cache" -Recurse | Remove-Item -Force -Recurse -WhatIf
-Get-ChildItem -Path "$env:public\..\*\AppData\Local\Microsoft\Windows\Temporary Internet Files" -Recurse | Remove-Item -Force -Recurse -WhatIf
-Get-ChildItem -Path "$env:public\..\*\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache2" -Recurse | Remove-Item -Force -Recurse -WhatIf
+Get-ChildItem -Path "$env:public\..\*\AppData\Local\Google\Chrome\User Data\Default\Cache" -Recurse | Remove-Item -Verbose -Force -Recurse -WhatIf >> "$env:windir\Logs\del.log"
+Get-ChildItem -Path "$env:public\..\*\AppData\Local\Microsoft\Windows\Temporary Internet Files" -Recurse | Remove-Item -Verbose -Force -Recurse -WhatIf >> "$env:windir\Logs\del.log"
+Get-ChildItem -Path "$env:public\..\*\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache2" -Recurse | Remove-Item -Verbose -Force -Recurse -WhatIf >> "$env:windir\Logs\del.log"
 
 #cmd:
 #forfiles.exe -p %temp% -s -m * -d -70 -c "cmd /c echo @path"
