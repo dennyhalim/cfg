2ac9fba5043e502f120468e981087259fefec7b8
DennyHalim.com <mail2dny@gmail.com>
2018-05-24 14:01:30 +0700

    Update SetupComplete.cmd

--- unattended.win10/Setup/Scripts/SetupComplete.cmd
+++ unattended.win10/Setup/Scripts/SetupComplete.cmd
@@ -42,6 +42,12 @@ schtasks.exe /change /TN "\Microsoft\Windows\Application Experience\ProgramDataU
 schtasks.exe /change /TN "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /DISABLE
 schtasks.exe /change /TN "\Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /DISABLE
 schtasks.exe /change /TN "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /DISABLE
+dism.exe /online /norestart /Enable-Feature /FeatureName:TelnetClient
+dism.exe /online /norestart /Disable-Feature /FeatureName:MediaPlayback
+dism.exe /online /norestart /Disable-Feature /FeatureName:WindowsMediaPlayer
+dism.exe /online /norestart /Disable-Feature /FeatureName:Indexing-Service-Package
+dism.exe /online /norestart /Disable-Feature /FeatureName:Internet-Explorer-Optional-x86
+dism.exe /online /norestart /Disable-Feature /FeatureName:Internet-Explorer-Optional-amd64
 
 "%WINDIR%\Setup\Files\chocolatey.exe" /S
 "%WINDIR%\Setup\Files\Firefox Setup 52.6.0esr.exe" -ms
