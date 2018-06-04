f1248934c6109e5ec1e6ab605ba7f239c4ff118d
DennyHalim.com <mail2dny@gmail.com>
2018-06-04 10:06:26 +0700

    Update SetupComplete.cmd

--- unattended.win10/Setup/Scripts/SetupComplete.cmd
+++ unattended.win10/Setup/Scripts/SetupComplete.cmd
@@ -10,8 +10,8 @@ rem move "%windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy" "%windir%
 rem startmenu disabled if shellex disabled
 rem move "%windir%\SystemApps\ShellExperienceHost_cw5n1h2txyewy" "%windir%\SystemApps\ShellExperienceHost_cw5n1h2txyewy.dennyhalim"
 
-wmic USERACCOUNT WHERE "Name='admin'" set PasswordExpires=FALSE
-wmic USERACCOUNT WHERE "Name='user'" set PasswordExpires=FALSE
+wmic.exe USERACCOUNT WHERE "Name='admin'" set PasswordExpires=FALSE
+wmic.exe USERACCOUNT WHERE "Name='user'" set PasswordExpires=FALSE
 
 reg.exe load HKLM\DEFAULT "%Public%\..\default\ntuser.dat"
 reg.exe add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People /v PeopleBand /t REG_DWORD /d 0 /f
