b1c89df4d1c3d410f4f96d36575602141be4bc61
DennyHalim.com <mail2dny@gmail.com>
2018-12-13 09:58:31 +0700

    Update redirect.php

--- www/redirect.php
+++ www/redirect.php
@@ -3,15 +3,11 @@
 <!-- use meta refresh -->
 <meta http-equiv="refresh" content="1;url= http://it.dennyhalim.com/?utm_source=old.dennyhalim.com">
 <!-- use javascript -->
-<script type="text/javascript">
-top.location = "http://it.dennyhalim.com/?utm_source=old.dennyhalim.com";
-</script>
+<script type="text/javascript"> top.location = "http://it.dennyhalim.com/?utm_source=old.dennyhalim.com"; </script>
 </head>
 <body>
 <!-- use iframe -->
-<iframe src ="http://it.dennyhalim.com/?utm_source=old.dennyhalim.com" style="width:100%; height:100%; border:0" scrolling="yes" >
-</iframe>
+<iframe src ="http://it.dennyhalim.com/?utm_source=old.dennyhalim.com" style="width:100%; height:100%; border:0" scrolling="yes" ></iframe>
 </body>
 
-<?php
-header("Location: http://it.dennyhalim.com/?utm_source=old.dennyhalim.com");
+<?php header("Location: http://it.dennyhalim.com/?utm_source=old.dennyhalim.com");
