7c00ffde5193eafa652cb03fe6567e655d024dc2
DennyHalim.com <mail2dny@gmail.com>
2019-03-13 14:36:41 +0700

    Update firefox-pref.js

--- apps/firefox-pref.js
+++ apps/firefox-pref.js
@@ -1,2 +1,13 @@
+// Mozilla User Preferences
+//dennyhalim.com
+pref("app.update.channel", "esr");
 user_pref("devtools.memory.enabled", false);
 user_pref("browser.cache.memory.enable", false);
+user_pref("network.cookie.cookieBehavior", 3);
+user_pref("network.cookie.lifetimePolicy", 2);
+user_pref("privacy.clearOnShutdown.offlineApps", true);
+user_pref("privacy.clearOnShutdown.siteSettings", true);
+user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("dom.webnotifications.enabled", false);
