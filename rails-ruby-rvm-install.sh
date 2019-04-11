ea47484d786149e89737a7c5f5a5afa530784e41
DennyHalim.com <mail2dny@gmail.com>
2019-04-11 12:42:47 +0700

    Update rails-ruby-rvm-install.sh

--- rails-ruby-rvm-install.sh
+++ rails-ruby-rvm-install.sh
@@ -3,6 +3,7 @@
 # sudo yum install -y autoconf automake bison gcc-c++ libtool readline-devel sqlite-devel zlib-devel openssl-devel
 # sudo yum install -y libffi-devel libyaml-devel ImageMagick-devel 
 # http://rvm.io/rvm/install
+# fix curl returned status '35': yum -y update nss*
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 source .rvm/scripts/rvm
