19e6a6b330c2a945031d681dc39fcfd377098652
DennyHalim.com <mail2dny@gmail.com>
2019-04-11 15:18:35 +0700

    Update rails-ruby-rvm-install.sh

--- rails-ruby-rvm-install.sh
+++ rails-ruby-rvm-install.sh
@@ -13,7 +13,7 @@ source .rvm/scripts/rvm
 rvm install 2.3
 rvm --default use 2.3
 gem install bundler -v 1.17
-gem install rails -v 4.2
+gem install rails -v 4.2.7.1
 
 #now you can install ruby/rails -based applications
 #tar zxvf redmine-3.3.5.tar.gz
