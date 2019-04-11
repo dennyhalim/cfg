b0da6e32afd0cf23539de7653ea4d3713ca4aadf
DennyHalim.com <mail2dny@gmail.com>
2019-04-11 10:31:10 +0700

    Update rails-ruby-rvm-install.sh

--- rails-ruby-rvm-install.sh
+++ rails-ruby-rvm-install.sh
@@ -1,7 +1,7 @@
 #dennyhalim.com
 # if dependencies is not installed, the user will need sudo yum privilege
-# sudo yum install -y autoconf automake bison gcc-c++ libffi-devel libtool readline-devel sqlite-devel zlib-devel libyaml-devel openssl-devel
-# sudo yum install -y ImageMagick-devel
+# sudo yum install -y autoconf automake bison gcc-c++ libtool readline-devel sqlite-devel zlib-devel openssl-devel
+# sudo yum install -y libffi-devel libyaml-devel ImageMagick-devel 
 # http://rvm.io/rvm/install
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
