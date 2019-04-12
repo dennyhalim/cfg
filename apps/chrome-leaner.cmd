7a7db9335be6354fa1b865b3e4caa09256da0ea4
DennyHalim.com <mail2dny@gmail.com>
2019-04-12 22:06:55 +0700

    Update chrome-leaner.cmd

--- apps/chrome-leaner.cmd
+++ apps/chrome-leaner.cmd
@@ -1,4 +1,5 @@
 rem dennyhalim.com
+rem run chrome-based browsers faster and using less process and memory
 cd %appdata%\google\chrome
 cd %programfiles%\google\chrome
 chrome.exe --process-per-site --renderer-process-limit=1 --disk-cache-size=1
