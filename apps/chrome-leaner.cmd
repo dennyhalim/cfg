093520cfdb52528b7e1bffc989b60cf0537561e6
DennyHalim.com <mail2dny@gmail.com>
2019-04-25 08:28:25 +0700

    Update chrome-leaner.cmd

--- apps/chrome-leaner.cmd
+++ apps/chrome-leaner.cmd
@@ -1,5 +1,6 @@
 rem dennyhalim.com
 rem run chrome-based browsers faster and using less process and memory
+rem might also works on chrome-based browsers and apps like opera, brave, iridium, epicbrowser, rambox, etc
 cd "%appdata%\google\chrome\application"
 cd "%programfiles%\google\chrome\application"
 cd "%programfiles(x86)%\google\chrome\application"
