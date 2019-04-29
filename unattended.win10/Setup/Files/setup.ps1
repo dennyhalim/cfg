ad486610e062b2bb763fcea1ab7e798b44c99786
DennyHalim.com <mail2dny@gmail.com>
2019-04-29 09:00:37 +0700

    Update setup.ps1

--- unattended.win10/Setup/Files/setup.ps1
+++ unattended.win10/Setup/Files/setup.ps1
@@ -17,7 +17,7 @@ $d.Attributes='Directory,NotContentIndexed'
 $d=get-item d:\
 $d.Attributes='Directory,NotContentIndexed'
 
-#unpin apps
+#unpin apps (must be run per user)
 (New-Object -Com Shell.Application).
     NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').
     Items() |
