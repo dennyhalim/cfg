d75c08ea6a5ac6775de5a9adbc779ab0c9ee588e
DennyHalim.com <mail2dny@gmail.com>
2019-04-29 08:57:15 +0700

    Update Startup.cmd

--- unattended.win10/Setup/Scripts/Startup.cmd
+++ unattended.win10/Setup/Scripts/Startup.cmd
@@ -8,3 +8,5 @@ reg.exe add "HKCU\Software\Microsoft\Input\TIPC" /v Enabled /t REG_DWORD /d 0 /f
 reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" /v GlobalUserDisabled /t REG_DWORD /d 1 /f
 reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\DeviceAccess\Global\{A8804298-2D5F-42E3-9531-9C8C39EB29CE}" /v Value /d Deny /f
 reg.exe delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v OneDriveSetup /F
+
+PowerShell.exe -ExecutionPolicy Bypass -File "%WINDIR%\Setup\Scripts\unpin-all.ps1"
