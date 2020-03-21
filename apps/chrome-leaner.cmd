d52a5b4b8f1db3a33220dcfd83423bdfb90455e3
DennyHalim.com <mail2dny@gmail.com>
2020-03-21 15:16:43 +0700

    Update chrome-leaner.cmd

--- apps/chrome-leaner.cmd
+++ apps/chrome-leaner.cmd
@@ -1,7 +1,10 @@
 rem dennyhalim.com
 rem run chrome-based browsers faster and using less process and memory
-rem might also works on chrome-based browsers and apps like opera, brave, iridium, epicbrowser, rambox, etc
+rem might also works on chrome-based browsers and apps like edge, opera, brave, iridium, epicbrowser, rambox, etc
 cd "%appdata%\google\chrome\application"
 cd "%programfiles%\google\chrome\application"
 cd "%programfiles(x86)%\google\chrome\application"
 chrome.exe --process-per-site --renderer-process-limit=1 --disk-cache-size=1
+
+#enable dns-over-https
+#enable this in chrome/opera/edge browser: chrome://flags/#dns-over-https
