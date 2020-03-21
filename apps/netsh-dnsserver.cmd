c78c7cf2ef456c04adbdbac67b1725d9dd126e31
DennyHalim.com <mail2dny@gmail.com>
2020-03-21 15:28:44 +0700

    Update netsh-dnsserver.cmd

--- apps/netsh-dnsserver.cmd
+++ apps/netsh-dnsserver.cmd
@@ -1,6 +1,7 @@
 rem netsh interface show interface
 rem https://cleanbrowsing.org/ip-address
 rem https://www.quad9.net/microsoft/
+rem wmic nicconfig where (IPEnabled=TRUE) call SetDNSServerSearchOrder ("185.228.168.10", "9.9.9.9")
 netsh interface ipv4 add dnsserver "Wi-Fi" address=185.228.168.10 index=1
 netsh interface ipv4 add dnsserver "Wi-Fi" address=9.9.9.9 index=2
 netsh interface ipv6 add dnsserver "Wi-Fi" address=2a0d:2a00:1::1 index=1
