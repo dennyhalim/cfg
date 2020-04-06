9bc651e9229341b06565864ded547e45921e3e56
DennyHalim.com <mail2dny@gmail.com>
2020-04-06 08:38:02 +0000

    Create fix-exefile-assoc.cmd

---
+++ windows/exefile-assoc/fix-exefile-assoc.cmd
@@ -0,0 +1,15 @@
+assoc.bat=batfile
+assoc.com=comfile
+assoc.cpl=cplfile
+assoc.Folder=Folder
+assoc.html=htmlfile
+assoc.ico=icofile
+assoc.lnk=lnkfile
+assoc.msi=Msi.Package
+assoc.reg=regfile
+assoc.scr=scrfile
+assoc.txt=txtfile
+assoc.URL=InternetShortcut
+assoc.vbs=VBSFile
+assoc.zip=CompressedFolder
+
