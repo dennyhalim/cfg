0152f2a6b011c8c0c4e4e060383daac843473e05
DennyHalim.com <mail2dny@gmail.com>
2020-12-18 14:26:48 +0700

    Update rails-ruby-rvm-install.sh

--- rails-ruby-rvm-install.sh
+++ rails-ruby-rvm-install.sh
@@ -5,15 +5,17 @@
 # sudo yum install -y libffi-devel libyaml-devel ImageMagick-devel
 # http://rvm.io/rvm/install
 # fix curl returned status '35': yum -y update nss*
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+gpg --keyserver hkp://keys.gnupg.net:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+#gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 source .rvm/scripts/rvm
 
 
-rvm install 2.3
-rvm --default use 2.3
-gem install bundler -v 1.17
-gem install rails -v 4.2.7.1
+rvm install 2.6
+rvm use 2.6 --default
+gem install rails -v 5.2
+gem install bundler #-v 1.17
 
 #now you can install ruby/rails -based applications
 #tar zxvf redmine-3.3.5.tar.gz
