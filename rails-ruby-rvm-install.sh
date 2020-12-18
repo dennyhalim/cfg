bcb856a08b6af22f7438d9f4648476f8094603f6
DennyHalim.com <mail2dny@gmail.com>
2020-12-18 14:31:43 +0700

    Update rails-ruby-rvm-install.sh

--- rails-ruby-rvm-install.sh
+++ rails-ruby-rvm-install.sh
@@ -14,7 +14,7 @@ source .rvm/scripts/rvm
 
 rvm install 2.6
 rvm use 2.6 --default
-gem install rails -v 5.2
+gem install rails -v 5.2.4.4
 gem install bundler #-v 1.17
 
 #now you can install ruby/rails -based applications
