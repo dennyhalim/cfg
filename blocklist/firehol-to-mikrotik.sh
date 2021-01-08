0fc31ad1a140e78e4b34ae70d53b680d4c57b071
DennyHalim.com <mail2dny@gmail.com>
2021-01-08 01:26:01 +0000

    Update firehol-to-mikrotik.sh

--- blocklist/firehol-to-mikrotik.sh
+++ blocklist/firehol-to-mikrotik.sh
@@ -13,4 +13,4 @@ saveTo=/var/www/html/bl/firehol.rsc
 now=$(date);
 echo "# Generated on $now" > $saveTo
 echo "/ip firewall address-list" >> $saveTo
-wget -q -O - https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset | awk --posix '/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/ { print "add list=blacklist timeout=23h59m55s address=" $1 " comment=dennyhalim-firehol-script";}' >> $saveTo
+wget -q -O - https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset | awk --posix '/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/ { print "add list=blacklist timeout=23h59m50s address=" $1 " comment=dennyhalim-firehol-script";}' >> $saveTo
