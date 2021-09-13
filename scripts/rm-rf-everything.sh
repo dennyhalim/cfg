68a0969c46eae8888416d1432b60cd7727e6b895
DennyHalim.com <mail2dny@gmail.com>
2021-09-13 12:44:57 +0700

    Update rm-rf-everything.sh

--- scripts/rm-rf-everything.sh
+++ scripts/rm-rf-everything.sh
@@ -1,16 +1,22 @@
-#extreme warning !! test before use!! i will NOT be responsible for any deleted files!!
-#ALL AND EVERYTHING WILL BE REMOVED WITHOUT PROMPT!!!
+#extreme warning !! very destructive script!! test yourself before use!! i will NOT be responsible for any deleted files!!
+#ALL AND EVERYTHING WILL BE REMOVED WITHOUT PROMPT AND UNSTOPPABLE !!!
 #recommend to run it manually after extreme care look at the path you want to remove!!
+#https://yonglhuang.com/rm-file/
 
 if [ -z "$1" ]; then exit; fi
+echo destroying $1 in 10 second !! ctrl+c to cancel now !!
+sleep 10
+
 cd $1 || exit #avoid mistakenly run script
 
-mkdir /tmp/emptydir
-rsync -a --delete /tmp/emptydir  $1   &
+rm -rf $1/*   &
+ionice -c 3 rm -rf $1   &
+
+find $1/* -exec rm -rf {}   &
+find $1 -delete   &
 
 perl -e 'for(<*>){unlink}'   &
 perl -e 'for(<*>){((stat)[9]<(unlink))}'   &
 
-ionice -c 3 rm -rf $1   &
-
-find $1 -delete   &
+mkdir /tmp/emptydir
+rsync -a --delete /tmp/emptydir  $1   &
