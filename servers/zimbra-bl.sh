a5ece892543af8aaf5d178bd0a69d474da38100f
DennyHalim.com <mail2dny@gmail.com>
2022-02-18 08:00:50 +0700

    Update zimbra-bl.sh

--- servers/zimbra-bl.sh
+++ servers/zimbra-bl.sh
@@ -8,6 +8,14 @@ zimbraMtaRestriction "reject_rbl_client zen.spamhaus.org" \
 zimbraMtaRestriction "reject_rbl_client bl.spamcop.net" \
 zimbraMtaRestriction "reject_rbl_client dnsbl.sorbs.net" \
 zimbraMtaRestriction "reject_rbl_client cbl.abuseat.org" \
-zimbraMtaRestriction "reject_rbl_client dnsbl.njabl.org"
+zimbraMtaRestriction "reject_rbl_client dnsbl.njabl.org" \
+zimbraMtaRestriction "reject_rhsbl_client dbl.spamhaus.org" \
+zimbraMtaRestriction "reject_rhsbl_client multi.uribl.com" \
+zimbraMtaRestriction "reject_rhsbl_client multi.surbl.org" \
+zimbraMtaRestriction "reject_rhsbl_reverse_client dbl.spamhaus.org" \
+zimbraMtaRestriction "reject_rhsbl_sender multi.uribl.com" \
+zimbraMtaRestriction "reject_rhsbl_sender multi.surbl.org" \
+zimbraMtaRestriction "reject_rhsbl_sender rhsbl.sorbs.net" \
+zimbraMtaRestriction "reject_rhsbl_sender dbl.spamhaus.org"
 
 zmprov gacf | grep zimbraMtaRestriction
