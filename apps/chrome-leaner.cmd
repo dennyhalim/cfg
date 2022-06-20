b8134434290ff7a86bd717cab00dd67059ab29aa
DennyHalim.com <mail2dny@gmail.com>
2022-06-20 13:48:55 +0700

    Update chrome-leaner.cmd

--- apps/chrome-leaner.cmd
+++ apps/chrome-leaner.cmd
@@ -4,7 +4,7 @@ rem might also works on chrome-based browsers and apps like edge, opera, brave, 
 cd "%appdata%\google\chrome\application"
 cd "%programfiles%\google\chrome\application"
 cd "%programfiles(x86)%\google\chrome\application"
-chrome.exe --process-per-site --renderer-process-limit=1 --disk-cache-size=1
+chrome.exe --process-per-site --renderer-process-limit=1 --TotalMemoryLimitMb=1024 --disk-cache-size=123456789 
 
 rem #enable dns-over-https
 rem #enable this in chrome/opera/edge browser: chrome://flags/#dns-over-https
