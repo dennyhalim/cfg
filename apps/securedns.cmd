204af2283ed14dd48a46bd7446896d412b214d21
DennyHalim.com <mail2dny@gmail.com>
2022-08-28 18:51:58 +0700

    add ms edge

--- apps/securedns.cmd
+++ apps/securedns.cmd
@@ -1,3 +1,8 @@
 set secdns=https://doh-ch.blahdns.com/dns-query
-reg.exe add "HKLM\Software\Policies\Google\Chrome" /v DnsOverHttpsMode /f /t REG_SZ /d "automatic"
-reg.exe add "HKLM\Software\Policies\Google\Chrome" /v DnsOverHttpsTemplates /f /t REG_SZ /d %secdns%
+reg.exe add "HKLM\Software\Policies\Google\Chrome" /v DnsOverHttpsMode /f /t REG_SZ /d "automatic" /f
+reg.exe add "HKLM\Software\Policies\Google\Chrome" /v DnsOverHttpsTemplates /f /t REG_SZ /d "%secdns%" /f
+
+reg add "HKLM\Software\Policies\Microsoft\Edge" /v "BuiltInDnsClientEnabled" /t REG_DWORD /d "1" /f
+reg add "HKLM\Software\Policies\Microsoft\Edge" /v "DnsOverHttpsMode " /t REG_SZ /d "automatic" /f
+reg add "HKLM\Software\Policies\Microsoft\Edge" /v "DnsOverHttpsTemplates" /t REG_SZ /d "%secdns%" /f
+
