aecd1c3263698dc666c812d67bb582f1ec33ec2c
DennyHalim.com <mail2dny@gmail.com>
2022-08-28 18:53:21 +0700

    Update securedns.cmd

--- apps/securedns.cmd
+++ apps/securedns.cmd
@@ -1,6 +1,6 @@
 set secdns=https://doh-ch.blahdns.com/dns-query
-reg.exe add "HKLM\Software\Policies\Google\Chrome" /v "DnsOverHttpsMode" /f /t REG_SZ /d "automatic" /f
-reg.exe add "HKLM\Software\Policies\Google\Chrome" /v "DnsOverHttpsTemplates" /f /t REG_SZ /d "%secdns%" /f
+reg.exe add "HKLM\Software\Policies\Google\Chrome" /v "DnsOverHttpsMode" /f /t REG_SZ /d "automatic"
+reg.exe add "HKLM\Software\Policies\Google\Chrome" /v "DnsOverHttpsTemplates" /f /t REG_SZ /d "%secdns%"
 
 reg add "HKLM\Software\Policies\Microsoft\Edge" /v "BuiltInDnsClientEnabled" /f /t REG_DWORD /d "1"
 reg add "HKLM\Software\Policies\Microsoft\Edge" /v "DnsOverHttpsMode" /f /t REG_SZ /d "automatic"
