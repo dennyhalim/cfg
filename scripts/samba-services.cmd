0bcfe5f84b5aa2c53754053776948ffd36316777
DennyHalim.com <mail2dny@gmail.com>
2022-09-05 15:53:42 +0700

    Update samba-services.cmd

--- scripts/samba-services.cmd
+++ scripts/samba-services.cmd
@@ -7,3 +7,8 @@ reg.exe add "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /f
 
 rem audit samba1
 reg.exe add "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /f /v AuditSmb1Access /t REG_DWORD -d 1
+
+rem firewall block
+netsh advfirewall firewall add rule name="block-netbios-udp" protocol=udp dir=in action=block localport=135-139
+netsh advfirewall firewall add rule name="block-netbios-tcp" protocol=tcp dir=in action=block localport=135-139
+netsh advfirewall firewall add rule name="block-samba" protocol=tcp dir=in action=block localport=445
