c9e53971aad5c5dd1fe38fabdee4724ce2b2eb6b
DennyHalim.com <mail2dny@gmail.com>
2025-03-03 08:54:28 +0700

    Create limit-browsers.bat

---
+++ apps/limit-browsers.bat
@@ -0,0 +1,8 @@
+rem start browsers with lowest priority
+
+start "brave" /low /max "C:\Users\denny\AppData\Local\BraveSoftware\Brave-Browser\Application\brave.exe"
+start "ff" /low /max "C:\Program Files\Mozilla Firefox\firefox.exe"
+rem start "chrome" /low /max "c:\Program Files\Google\Chrome\Application\chrome.exe"
+
+rem from run, add prefix "cmd /c"
+rem cmd /c start "ff" /low /max "C:\Program Files\Mozilla Firefox\firefox.exe"
