748e9184a75d5d5e1f7e56c4e10d21ab7e66793c
DennyHalim.com <mail2dny@gmail.com>
2025-09-22 08:03:31 +0700

    Update fh2mtk.sh

--- blocklist/fh2mtk.sh
+++ blocklist/fh2mtk.sh
@@ -1,6 +1,6 @@
 #new
 saveTo=/var/www/html/bl
-wget -q -O - https://raw.githubusercontent.com/firehol/blocklist-ipsets/refs/heads/master/dshield_7d.netset | awk --posix '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\// { print "do { /ip firewall address-list add address=" $1 " list=daftarblokir timeout=6d comment=bl.dennyhalim.com } on-error={}";}' >> $saveTo/dshield.rsc
+wget -q -O - https://raw.githubusercontent.com/firehol/blocklist-ipsets/refs/heads/master/dshield_7d.netset | awk --posix '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\// { print "do { /ip firewall address-list add address=" $1 " list=daftarblokir timeout=6d23:59:59 comment=bl.dennyhalim.com } on-error={}";}' >> $saveTo/dshield.rsc
 wget -q -O - https://raw.githubusercontent.com/firehol/blocklist-ipsets/refs/heads/master/et_block.netset | awk --posix '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\// { print "do { /ip firewall address-list add address=" $1 " list=daftarblokir timeout=23:59:59 comment=bl.dennyhalim.com } on-error={}";}' >> $saveTo/etblock.rsc
 wget -q -O - https://raw.githubusercontent.com/firehol/blocklist-ipsets/refs/heads/master/firehol_webserver.netset | awk --posix '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\// { print "do { /ip firewall address-list add address=" $1 " list=daftarblokir timeout=23:59:59 comment=bl.dennyhalim.com } on-error={}";}' >> $saveTo/fhweb.rsc
 #iblocklist hijacked
