1ec57441c44d5bd0cd1344cc6b25039e5396899a
DennyHalim.com <mail2dny@gmail.com>
2026-02-26 14:24:35 +0700

    Update SetupComplete.cmd

--- unattended.win10/Setup/Scripts/SetupComplete.cmd
+++ unattended.win10/Setup/Scripts/SetupComplete.cmd
@@ -2,11 +2,9 @@ rem dennyhalim.com
 rem this will be copied after first reboot and then run after next reboot
 
 rem compacting folders
-compact.exe /f /c C:\ProgramData\Microsoft\ c:\Users "C:\Program Files (x86)" C:\Windows\Temp C:\Windows\assembly C:\Windows\ServiceProfiles\LocalService\AppData\Local\Temp\ C:\Windows\Installer
-compact.exe /f /c C:\ProgramData\Microsoft\* c:\Users\*
-compact.exe /f /c C:\Windows\WinSxS\
-compact.exe /f /c C:\Windows\System32\DriverStore\FileRepository\
-compact.exe /f /c "C:\Program Files\WindowsApps"
+compact.exe /f /i /c C:\ProgramData\Microsoft\ c:\Users "C:\Program Files (x86)" C:\Windows\Temp C:\Windows\assembly C:\Windows\ServiceProfiles\LocalService\AppData\Local\Temp\ C:\Windows\Installer
+compact.exe /f /i /c C:\ProgramData\Microsoft\* c:\Users\*
+compact.exe /f /i /c C:\Windows\WinSxS\ C:\Windows\System32\DriverStore\FileRepository\ "C:\Program Files\WindowsApps"
 
 rem replace defaultlayouts before it gets created
 rem move "%Public%\..\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml" "%Public%\..\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.dennyhalim" 
