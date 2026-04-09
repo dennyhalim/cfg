244cca21a5c54fef63aacad6b7db48e31a868064
DennyHalim.com <mail2dny@gmail.com>
2026-04-09 08:58:14 +0700

    Update chrome-leaner.cmd

--- windows/apps/chrome-leaner.cmd
+++ windows/apps/chrome-leaner.cmd
@@ -2,8 +2,8 @@ rem dennyhalim.com
 rem run chrome-based browsers faster and using less process and memory
 rem might also works on chrome-based browsers and apps like edge, opera, brave, iridium, epicbrowser, rambox, etc
 cd "%appdata%\google\chrome\application"
-cd "%programfiles%\google\chrome\application"
 cd "%programfiles(x86)%\google\chrome\application"
+cd "%programfiles%\google\chrome\application"
 chrome.exe --process-per-site --renderer-process-limit=1 --TotalMemoryLimitMb=1024 --disk-cache-size=123456789 
 rem chrome as app
 rem chrome.exe --start-maximized --app=https://denny.wordpress.com 
