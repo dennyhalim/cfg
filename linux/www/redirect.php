3c496e1591b0b0666b90c43b61ecff094109c0af
DennyHalim.com <mail2dny@gmail.com>
2026-05-25 21:06:43 +0700

    Update redirect.php

--- linux/www/redirect.php
+++ linux/www/redirect.php
@@ -12,6 +12,6 @@ header("Location: $redirto");
 </head>
 <body>
 <!-- use iframe -->
-<iframe src ="<?php echo $redirto ?>" style="width:100%; height:100%; border:0" scrolling="yes" ></iframe>
+<iframe src="<?php echo $redirto ?>" style="width:100%; height:100%; border:0" scrolling="yes" ></iframe>
 </body>
 </html>
