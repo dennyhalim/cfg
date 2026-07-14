6e2b3592ae680f1ef4fe5c2e100e5d209a898a04
denny <mail2dny@gmail.com>
2026-07-14 08:58:07 +0700

    Update redirect.php

--- linux/www/redirect.php
+++ linux/www/redirect.php
@@ -9,6 +9,9 @@ header("Location: $redirto");
 <meta http-equiv="refresh" content="1;url=<?php echo $redirto; ?>">
 <!-- use javascript -->
 <script type="text/javascript"> top.location="<?php echo $redirto; ?>"; </script>
+  <script>window.goatcounter={path:function(p){return location.host+p}}</script>
+  <script data-goatcounter="https://mypolaris.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
 </head>
 <body id="denny.wordpress.com">
 <!-- use iframe -->
