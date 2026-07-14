b1d570fa92b1453f0f3e7258c08fa99f362b9669
denny <mail2dny@gmail.com>
2026-07-14 09:49:30 +0700

    Update redirect.php

--- linux/www/redirect.php
+++ linux/www/redirect.php
@@ -9,17 +9,17 @@ header("Location: $redirto");
 <html><head>
 <base target="_top">
 <!-- denny.wordpress.com html redirects -->
+  <script>window.goatcounter={path:function(p){return location.host+p}}</script>
+  <script data-goatcounter="https://mypolaris.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
 <!-- use meta refresh -->
 <meta http-equiv="refresh" content="1;url=<?php echo $redirto; ?>">
 <!-- use javascript -->
 <script type="text/javascript"> top.location="<?php echo $redirto; ?>"; </script>
-  <script>window.goatcounter={path:function(p){return location.host+p}}</script>
-  <script data-goatcounter="https://mypolaris.goatcounter.com/count"
-        async src="//gc.zgo.at/count.js"></script>
 </head>
 <body id="denny.wordpress.com">
 <!-- use iframe -->
 <iframe src="<?php echo $redirto; ?>" style="width:100%; height:100%; border:0" scrolling="yes" ></iframe>
-<a href="https://mypolaris.com">Redirected by Polaris Network Tools</a>
+<a href="https://mypolaris.com">PHP Redirect by Polaris Network Tools</a>
 </body>
 </html>
