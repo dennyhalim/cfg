d8b6bb1d03b4060fa68aa78971521307d90f978a
denny <mail2dny@gmail.com>
2026-09-18 11:29:48 +0700

    Update fh2mtk.sh

--- blocklist/fh2mtk.sh
+++ blocklist/fh2mtk.sh
@@ -1,4 +1,10 @@
 #new
+
+echo UPDATE!
+echo ====================================================
+echo check out http://bl.dennyhalim.com for latest update
+echo ====================================================
+
 saveTo=/var/www/html/bl
 wget -q -O - https://raw.githubusercontent.com/firehol/blocklist-ipsets/refs/heads/master/dshield_7d.netset | awk --posix '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\// { print "do { /ip firewall address-list add address=" $1 " list=daftarblokir timeout=6d23:59:59 comment=dshield.bl.dennyhalim.com } on-error={}";}' > $saveTo/dshield_7d.rsc
 #careful fh1 might block too much local networks
